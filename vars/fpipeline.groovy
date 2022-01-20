e7b07596a34f9aa174633c3b8eb67d42c209f063
Jesus <hiperion_xx@hotmail.com>
2022-01-20 12:18:19 -0300

    v19 segundo jenkinsfile name=fpipeline adicion post task pull request

--- vars/fpipeline.groovy
+++ vars/fpipeline.groovy
@@ -57,7 +57,7 @@ pipeline {
             sh "curl \
                 -XPUT \
                 -H "Accept 'application/vnd.github.v3+json'" \
-                -H "Authorization token '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'"
+                -H "Authorization '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'"
                 https://api.github.com/repos/jesusdonoso/ms-iclab/pulls \
                 -d '{"feature-estadomundial","develop"}'"
         }
