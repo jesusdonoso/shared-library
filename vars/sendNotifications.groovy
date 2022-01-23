056224d42e3042aed093bcf3dba55b07f05adec4
Jesus <hiperion_xx@hotmail.com>
2022-01-23 20:08:49 -0300

    modificacion de slack v17

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -2,16 +2,10 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend (color: "good", message: "[Grupo1][${env.JOB_NAME}][${env.GIT_BRANCH}][Resultado: OK]")
+    slackSend (color: "good", message: "[Grupo1][${env.JOB_NAME}][$${env.BRANCH_NAME}][Resultado: OK]")
   }
-  else if( buildResult == "FAILURE" ) {
-    slackSend (color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed")
-  }
-  else if( buildResult == "UNSTABLE" ) {
-    slackSend (color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable")
-  }
-  else {
-    slackSend (color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear")
+  else ( buildResult == "FAILURE" ) {
+    slackSend (color: "good", message: "[Grupo1][${env.JOB_NAME}][${env.GIT_BRANCH}][Resultado: Failed]")
   }
 }
 
