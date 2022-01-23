a4dbe782469334dddebb9d68c9fbc2b427b71941
Jesus <hiperion_xx@hotmail.com>
2022-01-23 18:50:31 -0300

    modificacion de slack

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,9 +2,15 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend (message "paso la wea")
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
   }
-  else ( buildResult == "FAILURE" ) {
-    slackSend (message "falla")
+  else if( buildResult == "FAILURE" ) { 
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+  }
+  else {
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
   }
 }
