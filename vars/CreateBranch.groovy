a43bb10fb36df8cfb22b38fda669c9345476e9b0
Jesus <hiperion_xx@hotmail.com>
2022-01-23 20:36:51 -0300

    modificacion de slack v20

---
+++ vars/CreateBranch.groovy
@@ -0,0 +1,8 @@
+if(env.BRANCH_NAME == 'master'){
+     stage("Upload"){
+        // Artifact repository upload steps here
+        }
+     stage("Deploy"){
+        // Deploy steps here
+       }
+     }
