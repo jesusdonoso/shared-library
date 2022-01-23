c5c1282c2f4c739a0423f9769f8eb04b5bbdf42d
Jesus <hiperion_xx@hotmail.com>
2022-01-23 20:17:19 -0300

    modificacion de slack v19

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -2,7 +2,7 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend (color: "good", message: "[Grupo1][${env.JOB_NAME}][${env.BRANCH_NAME}][Resultado: OK]")
+    slackSend (color: "good", message: "[Grupo1][${env.JOB_NAME}][${BRANCH_NAME}][Resultado: OK]")
   }
   else ( buildResult == "FAILURE" ) {
     slackSend (color: "good", message: "[Grupo1][${env.JOB_NAME}][${env.GIT_BRANCH}][Resultado: Failed]")
