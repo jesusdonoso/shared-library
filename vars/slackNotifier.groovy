00b600725b037097c841e2a58ee60d468d6c6b34
Jesus <hiperion_xx@hotmail.com>
2022-01-23 18:17:46 -0300

    adicion de comunicacion via slack v3

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -7,10 +7,4 @@ def call(String buildResult) {
   else if( buildResult == "FAILURE" ) {
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
   }
-  else if( buildResult == "UNSTABLE" ) {
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-  }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-  }
 }
