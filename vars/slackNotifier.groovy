4148371f4c57d41bf6c36b0086972a4991add462
Jesus <hiperion_xx@hotmail.com>
2022-01-23 18:25:54 -0300

    adicion de comunicacion via slack v4

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -4,7 +4,7 @@ def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
     slackSend color: "good", message: "[grupo1][Job: ${env.JOB_NAME}][Resultado: OK] with buildnumber ${env.BUILD_NUMBER} was successful"
   }
-  else if( buildResult == "FAILURE" ) {
+  else ( buildResult == "FAILURE" ) {
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
   }
 }
