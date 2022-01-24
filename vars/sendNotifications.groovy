79ea9edaf08f154a654c961cadda0b55853bc3e8
Jesus <hiperion_xx@hotmail.com>
2022-01-24 10:36:26 -0300

    modificacion de falla en el nodo v10

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -4,7 +4,13 @@ def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
     slackSend (color: "good", message: "[Grupo1][${env.JOB_NAME}][${env.BRANCH_NAME}][Resultado: OK]")
   }
-  else {
+  else if( buildResult == "FAILURE" ) { 
     slackSend (color: "danger", message: "[Grupo1][${env.JOB_NAME}][${env.GIT_BRANCH}][Resultado: Failed][${FAILED_STAGE}]")
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend (color: "danger", message: "[Grupo1][${env.JOB_NAME}][${env.GIT_BRANCH}][Resultado: Unstable][${FAILED_STAGE}]")
+  }
+  else {
+    slackSend (color: "danger", message: "[Grupo1][${env.JOB_NAME}][${env.GIT_BRANCH}][Resultado: Unclear][${FAILED_STAGE}]")
   }
 }
