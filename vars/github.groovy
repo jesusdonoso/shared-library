31a39df88c196eca6215e5a50087d189c4dd3c2f
Jesus <hiperion_xx@hotmail.com>
2022-01-24 18:57:21 -0300

    modificacion de falla en el nodo v45

--- vars/github.groovy
+++ vars/github.groovy
@@ -32,7 +32,7 @@ def createBranch() {
     SHA = sh (
         script:
             '''
-                curl https://api.github.com/repos/jesusdonoso/ejemplo-maven/git/refs/heads/shared-library | jq '.object.sha'
+                curl https://api.github.com/repos/jesusdonoso/ejemplo-maven/git/refs/heads/shared-library | jq -r '.object.sha'
             ''',
         returnStdout: true
     ).trim()
@@ -40,7 +40,7 @@ def createBranch() {
     print (SHA)
 
     sh '''
-        curl -s -X POST -H "Authorization: token $JENKINSTOKEN" -H "Accept 'application/vnd.github.v3+json'" -d '{\"ref\": \"refs/heads/prueba-rama\",\"sha\": $SHA}'  https://api.github.com/repos/jesusdonoso/ejemplo-maven/git/refs
+        curl -X POST -H "Authorization: token $JENKINSTOKEN" -H "Accept 'application/vnd.github.v3+json'" -d '{"ref":"refs/heads/test-rama", "sha":"$SHA"}'  https://api.github.com/repos/jesusdonoso/ejemplo-maven/git/refs
     '''
     sh '''
         curl -X POST -d '{"head":"release","base":"shared-library"}' -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $JENKINSTOKEN" https://api.github.com/repos/jesusdonoso/ejemplo-maven/merges
