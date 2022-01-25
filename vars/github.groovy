70b85eab377167008eafeca349eb4aa612063f64
Jesus <hiperion_xx@hotmail.com>
2022-01-25 10:16:01 -0300

    modificacion de falla en el nodo v49

--- vars/github.groovy
+++ vars/github.groovy
@@ -40,7 +40,7 @@ def createBranch() {
     print (SHA)
 
     sh '''
-        curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $JENKINSTOKEN"  https://api.github.com/repos/jesusdonoso/ejemplo-maven/git/refs -d '{"ref": "refs/heads/test-rama", "sha": "$SHA"}'
+        curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $JENKINSTOKEN"  https://api.github.com/repos/jesusdonoso/ejemplo-maven/git/refs -d '{"ref": "refs/heads/test-rama", "sha": "83f8e7c125acdbf6fe15fe43b40d971dcc83d2bc"}'
     '''
 }
 
