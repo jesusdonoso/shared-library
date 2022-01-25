b6dead15449eb26f5a99c0c11f8750dfb59caccd
Jesus <hiperion_xx@hotmail.com>
2022-01-25 12:20:36 -0300

    modificacion de falla en el nodo v50

--- vars/github.groovy
+++ vars/github.groovy
@@ -39,15 +39,18 @@ def createBranch() {
 
     print (SHA)
 
-    sh '''
-        curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $JENKINSTOKEN"  https://api.github.com/repos/jesusdonoso/ejemplo-maven/git/refs -d '{"ref": "refs/heads/test-rama", "sha": "83f8e7c125acdbf6fe15fe43b40d971dcc83d2bc"}'
-    '''
+    sh (
+        script:
+        '''
+        curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $JENKINSTOKEN"  https://api.github.com/repos/jesusdonoso/ejemplo-maven/git/refs -d '{"ref": "refs/heads/test-rama1", "sha": "$SHA"}'
+    ''',
+    )
 }
 
 
 def MergeBranch() {
 
     sh '''
-        curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $JENKINSTOKEN" https://api.github.com/repos/jesusdonoso/ejemplo-maven/merges -d '{"head":"release","base":"shared-library"}'
+        curl -X POST -H "Accept 'application/vnd.github.v3+json'" -H "Authorization: token $JENKINSTOKEN" https://api.github.com/repos/jesusdonoso/ejemplo-maven/merges -d '{"head":"shared-library","base":"release"}'
     '''
 }
